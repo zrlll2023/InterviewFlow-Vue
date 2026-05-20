54e68b416f47f02c96d9cdd4e3533d7fabcba257
周睿 <1875858608@qq.com>
2026-05-20 16:49:04 +0800

    fix:无法添加pdf简历的问题,新增了用户登录页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/pages/LoginPage.vue')
+  },
+  {
     path: '/',
     component: () => import('@/layouts/MainLayout.vue'),
     redirect: '/resumes',
@@ -20,6 +25,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to) => {
+  const token = localStorage.getItem('token')
+  if (to.path !== '/login' && !token) {
+    return '/login'
+  }
+  if (to.path === '/login' && token) {
+    return '/'
+  }
 })
 
 export default router
